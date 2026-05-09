ce4b4d042eebe733c66ee2d26382c9eb2504fb4f
ennikyoungeun <ennikyoungeun@gmail.com>
2026-05-09 11:35:06 +0900

    자바시작,git ignore추가

---
+++ src/be_study/start/HelloWorld.java
@@ -0,0 +1,29 @@
+package be_study.start;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World !");
+		
+		//자바 코드 실행 : Ctrl + F11  //
+		
+		//주석 메모 역할
+		
+		//한줄주석
+		/*여러줄 주석
+		 *엔터 치면서 
+		 *다음줄 내려가도 
+		 *주석으로 인식*/
+		
+//		주석:ctrl + /
+//		ctrl + shift + c
+		
+		//sysout 입력 -> ctrl + space 자동완성
+		//System.out.println();
+		System.out.println();
+		
+	}
+
+}
