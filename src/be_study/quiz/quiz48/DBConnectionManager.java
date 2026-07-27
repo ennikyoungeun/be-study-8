cd4b14048f49f261c7604147f74fff508f1cd961
ennikyoungeun <ennikyoungeun@gmail.com>
2026-07-27 15:29:08 +0900

    퀴즈48 DB JOIN 조회

---
+++ src/be_study/quiz/quiz48/DBConnectionManager.java
@@ -0,0 +1,52 @@
+package be_study.quiz.quiz48;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnectionManager {
+
+	public static Connection connectDB() {
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String db_url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String db_id = "scott";
+		String db_pw = "tiger";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	public static void disconnectDB(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+
+		try {
+			if (rs != null)
+				rs.close();
+
+			if (pstmt != null)
+				pstmt.close();
+
+			if (conn != null)
+				conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
